a9626f30f26a93897cdedf0493a74c5fe7592588
Monika Ibana <monika@v1040-wn-rt-b-1-10.campus-dynamic.uwaterloo.ca>
2020-01-27 18:49:11 -0500

    add create patient to route

--- src/Routes.js
+++ src/Routes.js
@@ -3,14 +3,26 @@ import { Switch } from "react-router-dom";
 import AppliedRoute from "./components/AppliedRoute";
 import Login from "./containers/Login";
 import PatientList from "./containers/PatientList";
-import Home from "./containers/Home"
+import Home from "./containers/Home";
+import CreatePatient from "./containers/CreatePatient";
 
 export default function Routes({ appProps }) {
   return (
     <Switch>
       <AppliedRoute path="/" exact component={Home} appProps={appProps} />
       <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
-      <AppliedRoute path="/list" exact component={PatientList} appProps={appProps} />
+      <AppliedRoute
+        path="/list"
+        exact
+        component={PatientList}
+        appProps={appProps}
+      />
+      <AppliedRoute
+        path="/create"
+        exact
+        component={CreatePatient}
+        appProps={appProps}
+      />
     </Switch>
   );
 }
