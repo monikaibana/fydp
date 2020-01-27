28c21b206ef5fca8bc5fdb0d9a8d8a351341cecf
ameliagking <amelia@eco-energy.ca>
2020-01-26 19:48:26 -0500

    Added fields

--- src/containers/CreatePatient.js
+++ src/containers/CreatePatient.js
@@ -3,46 +3,99 @@
 import React from "react";
 import "antd/dist/antd.css";
 import "../styles/mainstyles.css";
-import { Form, Icon, Input, Button, Checkbox, Select } from "antd";
-
+import { Form, Icon, Input, Button, Checkbox, Select, Dropdown, Menu, message } from "antd";
 
 const { Option } = Select;
 
-class CreatePatientPage extends React.Component {
-	render() {
-         return (
-             <div className="create-patient-page">
-             <div className="create-patient-container">
-                 <h1>Add Patient</h1>
-                 		<Form.Item label="Name">
-				        </Form.Item>
-				        <Form.Item wrapperCol={{ span: 12, offset: 5 }}>
-				      	</Form.Item>
-
-				        <Form.Item label="Gender">
-				        </Form.Item>
-				        <Form.Item wrapperCol={{ span: 12, offset: 5 }}>
-				      	</Form.Item>
-						
-						<Form.Item label="Date of Birth">
-				        </Form.Item>
-				        <Form.Item wrapperCol={{ span: 12, offset: 5 }}>
-				      	</Form.Item>
-
-				      	<Form.Item label="PID">
-				        </Form.Item>
-				        <Form.Item wrapperCol={{ span: 12, offset: 5 }}>
-				      	</Form.Item>
-
-						<Form.Item label="Study Type">
-				        </Form.Item>
-				        <Form.Item wrapperCol={{ span: 12, offset: 5 }}>
-				      	</Form.Item>
-    			 <Button type="primary">Add Patient</Button>
-             </div>
-             </div>
-         );
-     }
+function handleChange(value) {
+  console.log(`selected ${value}`);
 }
 
-export default CreatePatientPage;
+class CreatePatientPage extends React.Component {
+
+handleSubmit = (e) => {
+      e.preventDefault();
+      this.props.form.validateFields((err, values) => {
+        if (!err) {
+          alert('Received values of form: ', values);
+        } else {
+          alert(err)
+        }
+      });
+    }
+
+render() {
+	const { getFieldDecorator } = this.props.form;
+    return (
+         <div className="create-patient-page">
+             <div className="create-patient-container">
+             	<h1>Add Patient</h1>
+             	<Form.Item>
+		            {getFieldDecorator("Name", {
+		                rules: [
+		                  { required: true,
+		                  	message: "Please input the patient's name" },
+		                  { validator: this.checkPatientName }
+		                ]
+		              })(
+		                <Input
+		                  prefix={<Icon type="user" style={{ fontSize: 13 }} />}
+		                  placeholder="Patient Name"
+		                />
+		              )}  
+				</Form.Item>
+				<Form.Item>
+		            {getFieldDecorator("PID", {
+		                rules: [
+		                  { required: true,
+		                  	message: "Please input the patient's PID" },
+		                  { validator: this.checkPID }
+		                ]
+		              })(
+		                <Input
+		                  prefix={<Icon type="number" style={{ fontSize: 13 }} />}
+		                  placeholder="PID"
+		                />
+		              )}
+				</Form.Item>
+				<Form.Item>
+		            {getFieldDecorator("Date of Birth", {
+		                rules: [
+		                  { required: true,
+		                  	message: "Please input the patient's date of birth" },
+		                  { validator: this.checkDOB }
+		                ]
+			             })(
+		                <Input
+		                  prefix={<Icon type="calendar" style={{ fontSize: 13 }} />}
+		                  placeholder="Date of Birth"
+		                />
+		            )}  
+				</Form.Item>
+				<Form.Item>
+					<Select defaultValue="Gender" style={{ width: 250 }} onChange={handleChange}>
+						<Option value="Male">Male</Option>
+					    <Option value="Female">Female</Option>
+					</Select>
+				</Form.Item>
+				<Form.Item>
+					<Select defaultValue="Study Type" style={{ width: 250 }} onChange={handleChange}>
+						<Option value="Initial Diagnostic study">Initial Diagnostic Study</Option>
+					    <Option value="Repeat Diagnostic Study">Repeat Diagnostic Study</Option>
+					    <Option value="Repeat Diagnostic Study">Repeat Diagnostic Study</Option>
+					    <Option value="CPAP Study">CPAP Study</Option>
+					    <Option value="BiPAP Study">BiPAP Study</Option>
+					    <Option value="Repeat Therapeutic Study">Repeat Therapeutic Study</Option>
+					    <Option value="Study to Assess Other Therapy">Study to Assess Other Therapy</Option>
+					</Select>
+				</Form.Item>
+    			<Form.Item>
+    				<Button type="primary">Add Patient</Button>
+				</Form.Item>
+             </div>
+         </div>
+    );
+	}
+}
+
+export default Form.create()(CreatePatientPage);
