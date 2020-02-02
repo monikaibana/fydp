788580873ebad25e4f3ac9a30891ed8ade5241fb
Monika Ibana <monika@Monikas-MacBook-Pro.local>
2020-02-02 14:55:43 -0500

    remove api config

--- src/routes/api-config.js
+++
@@ -1,9 +0,0 @@
-// require("dotenv").config();
-// var apigClientFactory = require("aws-api-gateway-client").default;
-
-// var apigClient = apigClientFactory.newClient({
-//   invokeUrl: process.env.BASE_URL,
-//   region: "us-east-1",
-//   accessKey: process.env.ACCESS_KEY,
-//   secretKey: process.env.SECRET_KEY
-// });
