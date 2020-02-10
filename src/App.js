8a051f1bbf914f47feb6ef53777f707796123fe8
Monika Ibana <monika@v1040-wn-rt-b-68-253.campus-dynamic.uwaterloo.ca>
2020-02-10 14:57:08 -0500

    trying to get routes to work

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Routes from "./Routes";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./styles/mainstyles.css";
 
 function App(props) {
@@ -10,9 +11,11 @@ function App(props) {
   // }
 
   return (
-    <div className="login-page">
-      <Routes appProps={{ isAuthenticated, userHasAuthenticated }} />
-    </div>
+    <Router basename={process.env.REACT_APP_PUBLIC_URL}>
+      <div className="login-page">
+        <Routes appProps={{ isAuthenticated, userHasAuthenticated }} />
+      </div>
+    </Router>
   );
 }
 
