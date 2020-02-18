5d170768169dfbd3e38c4ad461f90d8c74f4aaa1
Alex Naylor <alex.naylor@kik.com>
2020-02-18 18:19:25 -0500

    Patient info interp (#22)
    * Interpretation Information tab on patient info page
    
    * patient info tab update
    
    * back button
    
    * tab alignment
    
    * working on date format and alignment
    
    * Updated date fields
    
    Added slash fomatting to date fields to be consistent with create patient page
    
    Co-authored-by: ameliagking <amelia@eco-energy.ca>
    Co-authored-by: monikaibana <38916978+monikaibana@users.noreply.github.com>

--- src/containers/PatientInfo.js
+++ src/containers/PatientInfo.js
@@ -1,5 +1,3 @@
-// Placeholder file for Patient Information Page
-
 import React from "react";
 import { Tabs, Icon, Select, Form, Input, Button } from "antd";
 import "../styles/PatientInfoStyles.css";
@@ -10,12 +8,13 @@ function handleChange(value) {
   console.log(`selected ${value}`);
 }
 const { TabPane } = Tabs;
-const {TextArea} = Input;
+const { TextArea } = Input;
 function callback(key) {
   console.log(key);
 }
 
 class PatientInfoPage extends React.Component {
+  
   state = {
     key: "Patient_Information"
   };
@@ -25,11 +24,28 @@ class PatientInfoPage extends React.Component {
     this.setState({ [type]: key });
   };
 
+  handleDateChange = e => {
+    e.target.value = e.target.value
+      .replace(/^(\d\d)(\d)$/g, "$1/$2")
+      .replace(/^(\d\d\/\d\d)(\d+)$/g, "$1/$2")
+      .replace(/[^\d/]/g, "");
+  };
+
+  goBack() {
+    this.props.history.goBack();
+  };
+
   render() {
+    const {getFieldDecorator} = this.props.form;
+
     return (
       <div className="PatientInfoPage">
         <Sidebar value={"PatientInfo"} />
-        <Button type="link" className="BackButton" href="javascript:history.back()">
+        <Button
+          type="link"
+          className="BackButton"
+          onClick={() => this.goBack()}
+        >
           <Icon type="left" /> Back
         </Button>
         <div className="PatientName">Patient Name</div>
@@ -88,97 +104,106 @@ class PatientInfoPage extends React.Component {
             <TabPane tab="Study Results" key="3">
               Content of Tab Pane 3
             </TabPane>
-            <TabPane tab="Interpretation Details" key="4" className="interpretationDetails">
-                <div className="InterpretationInfo">
-                  <div className="InterpretingDoctor">
-                    <h2>Interpreting Doctor <br/></h2>
-                    <Form.Item>
-                      <Select
-                        defaultValue="Select A Doctor"
-                        onChange={handleChange}
-                        style={{ width: 250 }}
-                      >
-                        <Option value="Raymond Gottschalk">
-                          Raymond Gottschalk
-                        </Option>
-                      </Select>
-                    </Form.Item>
-                  </div>
-                  <div className="InterpretationDate">
-                    <h2>Interpretation Date <br/></h2>
-                    <Form.Item>
+            <TabPane
+              tab="Interpretation Details"
+              key="4"
+              className="interpretationDetails"
+            >
+              <div className="InterpretationInfo">
+                <div className="InterpretingDoctor">
+                  <h2>
+                    Interpreting Doctor <br />
+                  </h2>
+                  <Form.Item>
+                    <Select
+                      defaultValue="Select A Doctor"
+                      onChange={handleChange}
+                      style={{ width: 250 }}
+                    >
+                      <Option value="Raymond Gottschalk">
+                        Raymond Gottschalk
+                      </Option>
+                    </Select>
+                  </Form.Item>
+                </div>
+                <div className="InterpretationDate">
+                  <h2>
+                    Interpretation Date <br />
+                  </h2>
+                  <Form.Item>
+                    {getFieldDecorator("interp_date")(
                       <Input
                         prefix={<Icon type="calendar" style={{ fontSize: 13 }} />}
                         placeholder="Interpretation Date (dd/mm/yyyy)"
+                        onChange={this.handleDateChange}
                         maxLength={10}
-                        style={{ width: 250 }}
+                        style={{ width: 300 }}
                       />
-                    </Form.Item>
-                  </div>
-                  <div className="StudyLink">
-                    <h2>Link To Study <br/></h2>
-                    <p> Link </p>
-                  </div>
+                    )}
+                  </Form.Item>
                 </div>
-                <div className="Interpretation">
-                  <div className="Rating">
-                    <h2>Rating <br/> </h2>
-                    <Form.Item>
-                      <Select
-                        defaultValue="Select A Rating"
-                        onChange={handleChange}
-                        style={{ width: 250 }}
-                      >
-                        <Option value="1">
-                          1
-                        </Option>
-                        <Option value="2">
-                          2
-                        </Option>
-                        <Option value="3">
-                          3
-                        </Option>
-                        <Option value="4">
-                          4
-                        </Option>
-                        <Option value="5">
-                          5
-                        </Option>
-                      </Select>
-                    </Form.Item>
-                  </div>
-                  <div className="reportSendDate">
-                    <h2>Date Report Sent <br/> </h2>
-                    <Form.Item>
+                <div className="StudyLink">
+                  <h2>
+                    Link To Study <br />
+                  </h2>
+                  <p> Link </p>
+                </div>
+              </div>
+              <div className="Interpretation">
+                <div className="Rating">
+                  <h2>
+                    Rating <br />{" "}
+                  </h2>
+                  <Form.Item>
+                    <Select
+                      defaultValue="Select A Rating"
+                      onChange={handleChange}
+                      style={{ width: 250 }}
+                    >
+                      <Option value="1">1</Option>
+                      <Option value="2">2</Option>
+                      <Option value="3">3</Option>
+                      <Option value="4">4</Option>
+                      <Option value="5">5</Option>
+                    </Select>
+                  </Form.Item>
+                </div>
+                <div className="reportSendDate">
+                  <h2>
+                    Date Report Sent <br />{" "}
+                  </h2>
+                  <Form.Item>
+                    {getFieldDecorator("sent_date")(
                       <Input
                         prefix={<Icon type="calendar" style={{ fontSize: 13 }} />}
                         placeholder="Sent Date (dd/mm/yyyy)"
+                        onChange={this.handleDateChange}
                         maxLength={10}
-                        style={{ width: 250 }}
+                        style={{ width: 300 }}
                       />
-                    </Form.Item>
-                  </div>
-                  <div className="Comments">
-                    <h2> Comments <br/> </h2>
-                    <Form.Item>
-                      <TextArea
-                        style={{ fontSize: 13 }, {width: 400}}
-                        placeholder="Comments"
-                        autoSize={{ minRows: 2, maxRows: 6 }}
-                      />
-                    </Form.Item>
-                  </div>
+                    )}
+                  </Form.Item>
                 </div>
+                <div className="Comments">
+                  <h2>
+                    {" "}
+                    Comments <br />{" "}
+                  </h2>
+                  <Form.Item>
+                    <TextArea
+                      style={({ fontSize: 13 }, { width: 400 })}
+                      placeholder="Comments"
+                      autoSize={{ minRows: 2, maxRows: 6 }}
+                    />
+                  </Form.Item>
+                </div>
+              </div>
             </TabPane>
             <TabPane tab="Post-Study Details" key="5">
               Content of Tab Pane 3
             </TabPane>
           </Tabs>
-          <Button
-            type="primary"
-            htmlType="submit"
-            className="save-button"
-          >
+          <Button type="primary" htmlType="submit" className="save-button">
             Save
           </Button>
         </div>
@@ -187,4 +212,4 @@ class PatientInfoPage extends React.Component {
   }
 }
 
-export default PatientInfoPage;
+export default Form.create()(PatientInfoPage);
