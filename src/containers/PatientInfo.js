9355c7701d56d9517ce0023b7037b887a226d954
Alex Naylor <alex.naylor@kik.com>
2020-02-25 14:17:19 -0500

    patient info page updates (#26)
    * update to bottom buttons to match design
    
    * Post study details tab
    
    * update to interpretation details tab
    
    * Study Results Tab
    
    * referring physician searchable select
    
    * fixed select values on ref. physician

--- src/containers/PatientInfo.js
+++ src/containers/PatientInfo.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Tabs, Icon, Select, Form, Input, Button } from "antd";
+import { Tabs, Icon, Select, Form, Input, Button, DatePicker, Checkbox } from "antd";
 import "../styles/PatientInfoStyles.css";
 import Sidebar from "../components/Sidebar.js";
+import moment from 'moment';
 
 const { Option } = Select;
 function handleChange(value) {
@@ -11,6 +12,24 @@ const { TabPane } = Tabs;
 const { TextArea } = Input;
 function callback(key) {
   console.log(key);
+}
+
+const dateFormat = 'DD/MM/YYYY';
+
+function onCheck(e) {
+  console.log(`checked = ${e.target.checked}`);
+}
+
+function onBlur() {
+  console.log('blur');
+}
+
+function onFocus() {
+  console.log('focus');
+}
+
+function onSearch(val) {
+  console.log('search:', val);
 }
 
 class PatientInfoPage extends React.Component {
@@ -34,6 +53,7 @@ class PatientInfoPage extends React.Component {
   goBack() {
     this.props.history.goBack();
   };
+  
 
   render() {
     const {getFieldDecorator} = this.props.form;
@@ -53,44 +73,11 @@ class PatientInfoPage extends React.Component {
           <div className="PatientStatus">
             Status
             <br />
-            <Form.Item>
-              <Select
-                defaultValue="Patient Status"
-                onChange={handleChange}
-                style={{ width: 250 }}
-              >
-                <Option value="Referral Received/For Triage">
-                  Referral Received/For Triage
-                </Option>
-                <Option value="Triaged">Triaged</Option>
-                <Option value="Consultation Booked">Consultation Booked</Option>
-                <Option value="Consultation Complete">
-                  Consultation Complete
-                </Option>
-                <Option value="Study Booked">Study Booked</Option>
-                <Option value="Study Data Collected">
-                  Study Data Collected
-                </Option>
-                <Option value="Study Scored">Study Scored</Option>
-                <Option value="Results Interpreted by Physician">
-                  Results Interpreted by Physician
-                </Option>
-                <Option value="Study Follow-up booked">
-                  Study Follow-up booked
-                </Option>
-                <Option value="Follow-up complete">Follow-up Complete</Option>
-                <Option value="Treatment Follow-up Booked">
-                  Treatment Follow-up Booked
-                </Option>
-                <Option value="Treatment Follow-up Complete">
-                  Treatment Follow-up Complete
-                </Option>
-              </Select>
-            </Form.Item>
+            <b>Patient Status</b>
           </div>
           <div className="TimeInStatus">
             Time in Status
-            <br />3 Days
+            <br /> <b>3 Days</b>
           </div>
         </div>
         <div className="InfoTabs">
@@ -158,7 +145,7 @@ class PatientInfoPage extends React.Component {
                   </Form.Item>
                 </div>
               </div>
-              <div className="Interpretation">
+              <div className="Triaging">
                 <div className="studyType">
                   <h2>
                     Study Type <br />{" "}
@@ -194,7 +181,7 @@ class PatientInfoPage extends React.Component {
                     )}
                   </Form.Item>
                 </div>
-                <div className="Comments">
+                <div className="Notes">
                   <h2>
                     {" "}
                     Notes <br />{" "}
@@ -206,7 +193,7 @@ class PatientInfoPage extends React.Component {
                       autoSize={{ minRows: 2, maxRows: 6 }}
                     />
                   </Form.Item>
-                    <div className="StudyLink">
+                    <div className="referralLink">
                     <h2>
                       Link To Referral File <br />
                     </h2>
@@ -305,15 +292,137 @@ class PatientInfoPage extends React.Component {
                 </div>
               </div>
             </TabPane>
-            <TabPane tab="Study Results" key="3">
-              Content of Tab Pane 3
+            <TabPane 
+            tab="Study Results" 
+            key="3"
+            className="studyResults"
+            >
+              <div className="scoring">
+                <div className="scorer">
+                  Scorer
+                  <Form.Item key="scorer">
+                  {getFieldDecorator("scorer", { initialValue: "0" })(
+                      <Select style={{ width: 300 }} onChange={handleChange}>
+                        <Option value="0" hidden>
+                          Scorer Name
+                        </Option>
+                        <Option value="1">Johnny</Option>
+                      </Select>
+                    )}
+                  </Form.Item>
+                </div>
+                <div className="scoreDate">
+                  Scored Date <br/>
+                  <DatePicker key="scoreDate" format={dateFormat} style={{width: 300}} defaultValue={moment()}/> <br/><br/>
+                </div>
+                <div className="ahi">
+                  AHI
+                  <Form.Item key="AHI">
+                    {getFieldDecorator("AHI")(
+                      <Input style={{width:300}} maxLength="2" placeholder="AHI"/>
+                    )}
+                  </Form.Item>
+                  REM AHI
+                  <Form.Item key="REMAHI">
+                    {getFieldDecorator("REMAHI")(
+                      <Input style={{width:300}} maxLength="2" placeholder="REM AHI"/>
+                    )}
+                  </Form.Item>
+                </div>
+                <div className= "score">
+                  Sleep Study Score
+                  <Form.Item key="studyScore">
+                  {getFieldDecorator("studyScore", { initialValue: "0" })(
+                      <Select style={{ width: 300 }} onChange={handleChange}>
+                        <Option value="0" hidden>
+                          Score
+                        </Option>
+                        <Option value="1">1</Option>
+                        <Option value="2">2</Option>
+                        <Option value="3">3</Option>
+                        <Option value="4">4</Option>
+                        <Option value="5">5</Option>
+                      </Select>
+                    )}
+                  </Form.Item>
+                </div>
+              </div>
+              <div className="additionalInfo">
+                <div className="studyTag">
+                  Study Tag
+                  <Form.Item key="studyTag">
+                    {getFieldDecorator("studyTag", {initialValue: "0"})(
+                      <Select style={{width:300}} onChange={handleChange}>
+                        <Option value="0">None</Option>
+                        <Option value="1">Tag 1</Option>
+                        <Option value="2">Tag 2</Option>
+                        <Option value="3">Tag 3</Option>
+                      </Select>
+                    )}
+                  </Form.Item>
+                </div>
+                <div className="referringPhysician">
+                  Referring Physician
+                  <Form.Item key="referringPhysician">
+                    {getFieldDecorator("referringPhysician")(
+                      <Select
+                      showSearch
+                      style={{ width: 300 }}
+                      placeholder="Referring Physician"
+                      optionFilterProp="children"
+                      onChange={handleChange}
+                      onFocus={onFocus}
+                      onBlur={onBlur}
+                      onSearch={onSearch}
+                      filterOption={(input, option) =>
+                        option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                      }
+                    >
+                      <Option value="Dr. Goodman">Dr. Goodman</Option>
+                      <Option value="Dr. Gooderman">Dr. Gooderman</Option>
+                      <Option value="Dr. Goodestman">Dr. Goodestman</Option>
+                    </Select>,
+                    )}
+                  </Form.Item>
+                </div>
+                <div className="link">
+                  Link to Study
+                  <p>Link</p>
+                </div>
+              </div>
             </TabPane>
             <TabPane
               tab="Interpretation Details"
               key="4"
               className="interpretationDetails"
             >
-              <div className="InterpretationInfo">
+                <div className="studyDets">
+                  <div className="AHI">
+                    <h2>
+                      AHI
+                    </h2>
+                    <b>25</b>
+                  </div>
+                  <div className="REMAHI">
+                    <h2>
+                      REM AHI
+                    </h2>
+                    <b>12</b>
+                  </div>
+                  <div className="studyScore">
+                    <h2>
+                      Sleep Study Score
+                    </h2>
+                    <b>4</b>
+                  </div>
+                  <div className="StudyLink">
+                    <h2>
+                      Link To Study <br />
+                    </h2>
+                    <p> Link </p>
+                  </div>
+                </div>
+                <div className="Interpretation">
                 <div className="InterpretingDoctor">
                   <h2>
                     Interpreting Doctor <br />
@@ -334,29 +443,11 @@ class PatientInfoPage extends React.Component {
                   <h2>
                     Interpretation Date <br />
                   </h2>
-                  <Form.Item>
-                    {getFieldDecorator("interp_date")(
-                      <Input
-                        prefix={<Icon type="calendar" style={{ fontSize: 13 }} />}
-                        placeholder="Interpretation Date (dd/mm/yyyy)"
-                        onChange={this.handleDateChange}
-                        maxLength={10}
-                        style={{ width: 300 }}
-                      />
-                    )}
-                  </Form.Item>
+                  <DatePicker key="interpretationDate" format={dateFormat} style={{width: 200}} defaultValue={moment()}/> <br/>
                 </div>
-                <div className="StudyLink">
-                  <h2>
-                    Link To Study <br />
-                  </h2>
-                  <p> Link </p>
-                </div>
-              </div>
-              <div className="Interpretation">
                 <div className="Rating">
                   <h2>
-                    Rating <br />{" "}
+                     Rating <br />{" "}
                   </h2>
                   <Form.Item>
                     <Select
@@ -372,26 +463,10 @@ class PatientInfoPage extends React.Component {
                     </Select>
                   </Form.Item>
                 </div>
-                <div className="reportSendDate">
-                  <h2>
-                    Date Report Sent <br />{" "}
-                  </h2>
-                  <Form.Item>
-                    {getFieldDecorator("sent_date")(
-                      <Input
-                        prefix={<Icon type="calendar" style={{ fontSize: 13 }} />}
-                        placeholder="Sent Date (dd/mm/yyyy)"
-                        onChange={this.handleDateChange}
-                        maxLength={10}
-                        style={{ width: 300 }}
-                      />
-                    )}
-                  </Form.Item>
-                </div>
+                <Checkbox className= "urgentAction" onChange={onCheck}>Urgent Action Required</Checkbox>
                 <div className="Comments">
-                  <h2>
-                    {" "}
-                    Comments <br />{" "}
+                  <h2> <br/>
+                    Special Comments <br />
                   </h2>
                   <Form.Item>
                     <TextArea
@@ -404,12 +479,63 @@ class PatientInfoPage extends React.Component {
               </div>
             </TabPane>
             <TabPane tab="Post-Study Details" key="5">
-              Content of Tab Pane 5
+              <div className="postStudyDetails">
+                <div className="reportSendDate">
+                  Date Report Sent <br/>
+                  <DatePicker key="reportSendDate" format={dateFormat} style={{width: 200}} defaultValue={moment()}/> <br/><br/>
+                  <Checkbox onChange={onCheck}>Study Billed</Checkbox>
+                </div>
+              </div>
             </TabPane>
           </Tabs>
-          <Button type="primary" htmlType="submit" className="save-button">
+          <div className="bottomButtons">
+          <Button type='danger' ghost htmlType="delete" className="archive-button">
+            Archive Patient
+          </Button>
+            <div className="changeStatus">
+            Change Status to: &nbsp;
+            </div>
+            <Form.Item className="statusUpdate">
+              <Select
+                defaultValue="Referral Received"
+                onChange={handleChange}
+                style={{width: 250}}
+              >
+                <Option value="Referral Received">
+                  Referral Received
+                </Option>
+                <Option value="Triaged">Triaged</Option>
+                <Option value="Consultation Booked">Consultation Booked</Option>
+                <Option value="Consultation Complete">
+                  Consultation Complete
+                </Option>
+                <Option value="Study Booked">Study Booked</Option>
+                <Option value="Study Data Collected">
+                  Study Data Collected
+                </Option>
+                <Option value="Study Scored">Study Scored</Option>
+                <Option value="Results Interpreted by Physician">
+                  Results Interpreted by Physician
+                </Option>
+                <Option value="Study Follow-up booked">
+                  Study Follow-up booked
+                </Option>
+                <Option value="Follow-up complete">Follow-up Complete</Option>
+                <Option value="Treatment Follow-up Booked">
+                  Treatment Follow-up Booked
+                </Option>
+                <Option value="Treatment Follow-up Complete">
+                  Treatment Follow-up Complete
+                </Option>
+              </Select>
+            </Form.Item>
+          <Button type="primary" htmlType="submit" className="save-button" style={{width: 75}}>
             Save
           </Button>
+          <Button type='normal' htmlType="cancel" className="cancel-button" style={{width: 75}}>
+            Cancel
+          </Button>
+          </div>
         </div>
       </div>
     );
