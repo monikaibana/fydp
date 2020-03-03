c3999ca068550a8c6b0b6f3bc5a3c9855890d9ab
ameliagking <amelia@eco-energy.ca>
2020-03-03 16:08:27 -0500

    Added first content box

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -8,6 +8,21 @@ class HomePage extends React.Component {
     return (
       <div className="Homepage">
         <div className="Background" />
+        <div className="MountainsPhoto" />
+        <div className="SloganContainerBackground"/>
+        <div className="SloganContainerContent">
+          <div className="SloganLogoText">
+            <h2>BlueBook</h2>
+          </div>
+          <div className="SloganLogoImage">
+            <img src={logo} alt="logo" style={{ height: 220 }}></img>
+          </div>
+          <div className="Slogan">
+            <h1>
+              The sleep clinic patient management tool that can help you and your patients sleep easier.
+            </h1>
+          </div>
+        </div>
         <div className="NavBar">
           <div className="NavBarContact">
             <Button type="secondary" size="large" href="contact-us" style={{ width: 150}}>
@@ -20,29 +35,8 @@ class HomePage extends React.Component {
             </Button>
           </div>
           <div className="NavBarLogo">
-            <Button>BlueBook</Button>
+            <h2>BlueBook</h2>
           </div>
-        </div>
-        <div className="MedicalPhoto" />
-        <img src={logo} alt="logo" style={{ height: 300 }}></img>
-        <div className="Introduction">
-          <h1>What is Bluebook? </h1>
-          <h3>
-            BlueBook is a digital patient management system which aims to reduce
-            the time taken by sleep clinic staff to track and monitor patient
-            status as they progress through the diagnosis process. Typically,
-            patient tracking at sleep clinics is completed manually, by editing
-            spreadsheets, and renaming and moving files. BlueBook’s simplified
-            tracking process is designed to reduce human errors and increase
-            clinic efficiency.
-            <br />
-            <br />
-            BlueBook is designed by 4th year engineering students at the
-            University of Waterloo.
-            <br />
-            <br />
-          </h3>
-
         </div>
       </div>
     );
