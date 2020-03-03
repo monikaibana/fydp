01f978e741a2ca1699f3fcb191e6ead07a1cf0c8
ameliagking <amelia@eco-energy.ca>
2020-03-03 17:15:04 -0500

    Added value propositions bar

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "../styles/homeStyles.css";
 import logo from "../placeholder.svg";
-import { Button } from "antd";
+import { Button, Icon } from "antd";
+//import { LikeOutlined, CheckCircleOutlined, SearchOutlined } from "@ant-design/icons";
 
 class HomePage extends React.Component {
   render() {
@@ -21,6 +22,41 @@ class HomePage extends React.Component {
             <h1>
               The sleep clinic patient management tool that can help you and your patients sleep easier.
             </h1>
+          </div>
+        </div>
+        <div className="ValuePropsContainer">
+          <div className="ValueProp1">
+            <div className="Icon1">
+              <Icon type="like" style={{ color: "#1890ff" }} />
+            </div>
+            <h2>
+              More Efficient
+            </h2>
+            <h5>
+              than tracking patients with traditional tracking methods
+            </h5>
+          </div>
+          <div className="ValueProp2">
+            <div className="Icon2">
+              <Icon type="check-circle" style={{ color: "#1890ff"}} />
+            </div>
+            <h2>
+              Designed
+            </h2>
+            <h5>
+              specifically for sleep clinics in Ontario with CPSO and OHIP guidleines in mind
+            </h5>
+          </div>
+          <div className="ValueProp3">
+            <div className="Icon3">
+              <Icon type="search" style={{ color: "#1890ff"}} />
+            </div>
+            <h2>
+              Analytics
+            </h2>
+            <h5>
+              to track and improve clinic performance
+            </h5>
           </div>
         </div>
         <div className="NavBar">
