6b18454a0bb132db90955559a47899d084ddc448
Monika Ibana <monika@v1040-wn-rt-b-127-234.campus-dynamic.uwaterloo.ca>
2020-03-08 21:15:38 -0400

    replaced monitoring with tracking

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,7 +2,11 @@ import React from "react";
 import "../styles/homeStyles.css";
 import logo from "../placeholder.svg";
 import { Button } from "antd";
-import { LikeOutlined, CheckCircleOutlined, SearchOutlined } from "@ant-design/icons";
+import {
+  LikeOutlined,
+  CheckCircleOutlined,
+  SearchOutlined
+} from "@ant-design/icons";
 
 class HomePage extends React.Component {
   render() {
@@ -16,17 +20,15 @@ class HomePage extends React.Component {
         <div className="MountainsPhoto1" />
         <div className="WhatContainer">
           <div className="WhatText">
-            <h1>
-              What is BlueBook?
-            </h1>
+            <h1>What is BlueBook?</h1>
             <h5>
-              BlueBook is a digital patient management system which 
-              aims to reduce the time taken by sleep clinic staff to track 
-              and monitor patient status as they progress through the diagnostic process. 
-              Typically, patient tracking at sleep clinics is completed manually, by 
-              editing spreadsheets, and renaming and moving files. BlueBook’s simplified
-              tracking process is designed to reduce human errors and increase clinic
-              efficiency.
+              BlueBook is a digital patient tracking system which aims to reduce
+              the time taken by sleep clinic staff to track and monitor patient
+              status as they progress through the diagnostic process. Typically,
+              patient tracking at sleep clinics is completed manually, by
+              editing spreadsheets, and renaming and moving files. BlueBook’s
+              simplified tracking process is designed to reduce human errors and
+              increase clinic efficiency.
             </h5>
           </div>
           <div className="WhatLogo">
@@ -34,23 +36,27 @@ class HomePage extends React.Component {
           </div>
         </div>
         <div className="AboutUsContainer">
-          <div className="AboutUsImage"/>
+          <div className="AboutUsImage" />
           <div className="AboutUsText">
-            <h1>
-              About Us
-            </h1>
+            <h1>About Us</h1>
             <h5>
               BlueBook was designed and built by Monika, Andy, Amelia, and Alex.
-              We are 4th year Systems Design engineering students at the University of Waterloo.
+              We are 4th year Systems Design engineering students at the
+              University of Waterloo.
             </h5>
           </div>
           <div className="ContactUs">
-            <Button type="secondary" size="large" href="mailto:info.mybluebook@gmail.com?subject=Mail from Our Site" style={{ width: 150}}>
+            <Button
+              type="secondary"
+              size="large"
+              href="mailto:info.mybluebook@gmail.com?subject=Mail from Our Site"
+              style={{ width: 150 }}
+            >
               Contact Us
             </Button>
           </div>
         </div>
-        <div className="SloganContainerBackground"/>
+        <div className="SloganContainerBackground" />
         <div className="SloganContainerContent">
           <div className="SloganLogoText">
             <h2>BlueBook</h2>
@@ -60,7 +66,8 @@ class HomePage extends React.Component {
           </div>
           <div className="Slogan">
             <h1>
-              The sleep clinic patient tracking tool that can help you and your patients sleep easier.
+              The sleep clinic patient tracking tool that can help you and your
+              patients sleep easier.
             </h1>
           </div>
         </div>
@@ -69,44 +76,45 @@ class HomePage extends React.Component {
             <div className="Icon1">
               <LikeOutlined style={{ color: "#1890ff" }} />
             </div>
-            <h2>
-              More Efficient
-            </h2>
-            <h5>
-              than tracking patients with traditional tracking methods
-            </h5>
+            <h2>More Efficient</h2>
+            <h5>than tracking patients with traditional tracking methods</h5>
           </div>
           <div className="ValueProp2">
             <div className="Icon2">
-              <CheckCircleOutlined style={{ color: "#1890ff"}} />
+              <CheckCircleOutlined style={{ color: "#1890ff" }} />
             </div>
-            <h2>
-              Designed
-            </h2>
+            <h2>Designed</h2>
             <h5>
-              specifically for sleep clinics in Ontario with CPSO and OHIP guidleines in mind
+              specifically for sleep clinics in Ontario with CPSO and OHIP
+              guidleines in mind
             </h5>
           </div>
           <div className="ValueProp3">
             <div className="Icon3">
-              <SearchOutlined style={{ color: "#1890ff"}} />
+              <SearchOutlined style={{ color: "#1890ff" }} />
             </div>
-            <h2>
-              Analytics
-            </h2>
-            <h5>
-              to track and improve clinic performance
-            </h5>
+            <h2>Analytics</h2>
+            <h5>to track and improve clinic performance</h5>
           </div>
         </div>
         <div className="NavBar">
           <div className="NavBarContact">
-            <Button type="secondary" size="large" href="mailto:info.mybluebook@gmail.com?subject=Mail from Our Site" style={{ width: 150}}>
+            <Button
+              type="secondary"
+              size="large"
+              href="mailto:info.mybluebook@gmail.com?subject=Mail from Our Site"
+              style={{ width: 150 }}
+            >
               Contact Us
             </Button>
           </div>
           <div className="NavBarLogin">
-            <Button type="primary" size="large" href="login" style={{ width: 150}}>
+            <Button
+              type="primary"
+              size="large"
+              href="login"
+              style={{ width: 150 }}
+            >
               Login
             </Button>
           </div>
